b4ba5af5076e575812c43ccebdecffeb4903d6af
Ajddn <92152971+Ajddn@users.noreply.github.com>
2023-08-08 19:06:39 +0530

    Update index.js
    changing FRONTEND_URL in uppercase

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // updating cors again
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', frontendUrl);
+  res.setHeader('Access-Control-Allow-Origin', FRONTEND_URL);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
