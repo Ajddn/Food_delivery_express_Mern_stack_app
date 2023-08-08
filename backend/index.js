7e14e03ec7e1daf59223ac3e7bc82f33e00cc9c3
Ajddn <92152971+Ajddn@users.noreply.github.com>
2023-08-08 22:56:22 +0530

    Update index.js
    deleting unecessary comment

--- backend/index.js
+++ backend/index.js
@@ -218,27 +218,6 @@ app.listen(PORT, () => console.log("server is running on port : " + PORT));
 
 
 
-// // This is your test secret API key.
-// const stripe = require('stripe')('sk_test_51NYtiuSG1VYaJyiMQ59klAqBtpQdgZNV6X2UgN5cnftwXkPdW9hFpLLTiOiHoGJKhGqq7ZFXu52RrG1iVrvOVq4H00xe7KqWRY');
-// const express = require('express');
-// const app = express();
-// app.use(express.static('public'));
-
-// const YOUR_DOMAIN = 'http://localhost:4242';
-
-// app.post('/create-checkout-session', async (req, res) => {
-//   const session = await stripe.checkout.sessions.create({
-//     line_items: [
-//       {
-//         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-//         price: '{{PRICE_ID}}',
-//         quantity: 1,
-//       },
-//     ],
-//     mode: 'payment',
-//     success_url: `${YOUR_DOMAIN}/success.html`,
-//     cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-//   });
 
 //   res.redirect(303, session.url);
 // });
