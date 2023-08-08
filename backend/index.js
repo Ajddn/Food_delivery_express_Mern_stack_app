aa4648389a26cbd38e50c11a75b89f9d66c2fe9e
Ajddn <92152971+Ajddn@users.noreply.github.com>
2023-08-08 22:51:50 +0530

    Update index.js
    Use the cors middleware

--- backend/index.js
+++ backend/index.js
@@ -10,24 +10,9 @@ const app = express();
 
 
 
-// Define the allowCors middleware
-const allowCors = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  next();
-};
 
-// Apply the allowCors middleware
-app.use(allowCors);
+// Use the cors middleware
+app.use(cors());
 
 
 
