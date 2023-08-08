e705be4bcb112defe4a75e1eb5c0696b41d09c3b
Ajddn <92152971+Ajddn@users.noreply.github.com>
2023-08-08 19:00:03 +0530

    Update index.js
    updating cors again

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,15 @@ const app = express();
 
 
 
-app.use(cors());
+
+// updating cors again
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', frontendUrl);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 
 
