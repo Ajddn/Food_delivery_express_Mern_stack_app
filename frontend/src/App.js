a8a9b98ec8e66d4cfd6d1e81e403145b0f219781
Ajddn <92152971+Ajddn@users.noreply.github.com>
2023-12-20 17:20:16 +0530

    Update App.js caching code added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,24 +9,61 @@ import { setDataProduct } from "./redux/productSlice";
 function App() {
   const dispatch = useDispatch();
   const productData = useSelector((state) => state.product);
+  // useEffect(() => {
+
+  //   (async () => {
+  //     const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/product`);
+  //     const resData = await res.json();
+  //     dispatch(setDataProduct(resData));
+  //   })();
+  // }, [dispatch]);
+
+
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const res = await fetch(
+  //         `${process.env.REACT_APP_SERVER_DOMAIN}/product`
+  //       );
+  //       if (!res.ok) {
+  //         throw new Error("Network response was not ok.");
+  //       }
+  //       const resData = await res.json();
+  //       console.log(resData); // Log the parsed JSON data
+  //       dispatch(setDataProduct(resData));
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   })();
+  // }, [dispatch]);
+
   useEffect(() => {
-    (async () => {
+    const fetchData = async () => {
       try {
-        const res = await fetch(
-          `${process.env.REACT_APP_SERVER_DOMAIN}/product`
-        );
+        const res = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/product`);
         if (!res.ok) {
           throw new Error("Network response was not ok.");
         }
         const resData = await res.json();
-        console.log(resData); // Log the parsed JSON data
+
+        // Preload image into the cache
+        resData.forEach((product) => {
+          const imageUrl = `${process.env.REACT_APP_SERVER_DOMAIN}/product`;
+          const img = new Image();
+          img.src = imageUrl;
+          img.onload = () => {
+            // Handle the image load (if needed)
+          };
+        });
+
         dispatch(setDataProduct(resData));
       } catch (error) {
         console.error("Error fetching data:", error);
       }
-    })();
-  }, [dispatch]);
+    };
 
+    fetchData();
+  }, [dispatch]);
 
   console.log(productData);
   return (
